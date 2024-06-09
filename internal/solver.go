fe6f8b93266c0bfaabc6036a5bd271910d3096cf
Rob Tipping <61379257+RobTipping@users.noreply.github.com>
2024-06-09 17:36:42 +0100

    added row and col check

--- internal/solver.go
+++ internal/solver.go
@@ -1,7 +1,39 @@
 package solver
 
-import "fmt"
 
-func ValidGrid() {
-	fmt.Println("this is a test")
+func ValidGrid(grid [9][9]int) bool{
+	for i:= range grid {
+		for j:= range grid[i] {
+			if grid[i][j] == 0 {
+				continue
+			}
+			// check rows
+			for k:= range grid[i] {
+				if j==k {
+					continue
+				}
+				if grid[i][j] == grid[i][k] {
+					return false
+				}
+				if i==k {
+					continue
+				}
+				if grid[i][j] == grid[k][j] {
+					return false
+				}
+			}
+			// check columns
+			/*
+			for k:= range grid {
+				if i==k {
+					continue
+				}
+				if grid[i][j] == grid[k][j] {
+					return false
+				}
+			}
+		*/
+		}
+	}
+	return true
 }
